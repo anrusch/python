5e07b21a0e703730cd31942b06324f896bff6132
anrusch <73241904+anrusch@users.noreply.github.com>
2020-12-15 21:05:09 -0600

    input
    12/15/20

---
+++ python restart/inputDrills.py
@@ -0,0 +1,55 @@
+'''
+For this assignment you should read the task, then below the task do what it 
+asks you to do.
+
+EXAMPLE TASK:
+'''
+#EX) Get input from the user to the question "What is your favorite color?"
+#and store their answer in a variable called favColor.
+favColor = input("What is your favorite color?")
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Get input from the user to the question "What is your favorite animal?"
+#and store their answer in a variable called favAnimal.
+text_to_user = input ("What is your favorite animal?")
+Favanimal= text_to_user
+
+
+#2) Get input from the user to the question "What is your favorite number?"
+#and store their answer in a variable called favNum. Convert favNum from a 
+#string variable to a int variable.
+text_to_user = input ("What is your favorite number?")
+Favnum= text_to_user
+1=Favnum
+
+#3) Get input from the user to the question "What is your school's name?"
+#and store their answer in a variable called school.
+text_to_user = input ("What is your schools name?")
+school= text_to_user
+
+
+#4) Get input from the user to the question "What is your instructors name?"
+#and store their answer in a variable called instructor.
+
+text_to_user = input ("What is your instructor's name?")
+instructor= text_to_user
+
+
+#5) Get input from the user to the question "What is 2 + 2?"
+#and store their answer in a variable called ans. Convert ans from a string
+#variable to a int variable
+text_to_user =input ("What is 2+2?")
+ans= text_to_user
+2=ans
+
+
+
+
+
---
+++ python restart/inputoutline.py
@@ -0,0 +1,32 @@
+'''
+This outline will help solidify concepts from the Variables lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Get input from the user to the question "What is your favorite color?"
+#and store their answer in a variable called favColor.
+text_to_user = input " What is your favorite color?"
+Favcolor = text_to_user
+
+
+print (response)
+
+#2) Make a variable called question and set it to "What is your name?". Then
+#get input from the user to the question and store their answer in a variable
+#called name.
+
+question = input ("What is your name")
+userResponse = text_to_user
+
+print (response)
+
+#3) Get input from the user to the question "What is your age?" and store their
+#answer in a variable called age. Convert age from a string variable to a int
+#(integer) variable.
+
+text_to_user = input ("What is your age?")
+age = input text_to_user
+1 = age 
+
+print (response)
+
