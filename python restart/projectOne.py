63e409221737b387cd2a8a110b25e86a795148a0
anrusch <73241904+anrusch@users.noreply.github.com>
2020-12-15 21:16:35 -0600

    Create projectOne.py
    12/15/20

---
+++ python restart/projectOne.py
@@ -0,0 +1,50 @@
+
+'''
+Created on Dec 13, 2020
+
+@author: Anna Rusch
+
+What the task is suppose to do is properly make a program that can take survey information
+from a user. By using and understanding oommenting, output, variables.
+
+#Use input() to ask the user "What is your name?" and store that in
+#a variable called name. Then print the variable.
+
+
+#Use input() to ask the user "What is your favorite color?" and store that
+#in a variable called color. Then print the variable.
+
+
+#Use input() to ask the user "What city did you grow up in?" and store
+#that in a variable called city. Then print the variable.
+
+
+#Use input() to ask the user "What is your best friends name?" and store
+#that in a variable called friend. Then print the variable.
+
+
+#Use input() to ask the user "What color hair do you have?" and store that
+#in a variable called hair. Then print the variable.
+
+'''
+text_to_user = input ("What is your name?")
+name = text_to_user
+print (name)
+
+text_to_user = input ("What is your favorite color?")
+color = text_to_user
+print (color)
+
+text_to_user = input ("What city did you grow up in?")
+city= text_to_user 
+print (city)
+
+text_to_user = input ("What is your best friend's name?")
+friend= text_to_user
+print (friend)
+
+text_to_user = input ("What color hair do you have?")
+colorHair = text_to_user
+print (colorHair)
+
+
